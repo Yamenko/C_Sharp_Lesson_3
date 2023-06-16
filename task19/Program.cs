2a8cf67d6488ac13a927123de067f20733e6ddb4
Me <1@1.ru>
2023-06-16 23:44:14 +0300

    задание 21

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задача 19: Напишите программу, которая принимает 
+//на вход пятизначное число и проверяет, является ли 
+//оно палиндромом.
+
+int num = 0, digit = 0;
+
+while (true) { 
+Console.WriteLine("Введите число (0 для выхода): ");
+
+	// Получаем число
+	if (!Int32.TryParse(Console.ReadLine(), out num))   { Console.WriteLine("Не число!"); continue; }
+	
+	// Условие выхода
+	if (num == 0) { break; }
+
+	// Проверка что число 5ти значное
+	if (num < 10000 && num >= 10000 )                   { Console.WriteLine("Не 5ти значное!"); continue; }
+
+	// Расчет разрядности
+	digit = (int)Math.Ceiling(Math.Log10(num) - 1);
+	bool polindrom = true;
+
+	// Прверка палиндрома (сравниваем по очереди разряды слева и справа, сложно N/2)
+	for (int i = 0; i < (digit / 2); i++) {
+		int left = (int)((num / Math.Pow(10, (digit - i))) % 10);
+		int right = (int)((num / Math.Pow(10, i)) % 10);
+		if (right != left) { polindrom = false; break; }
+	}
+
+	// Вывод результата
+	if (polindrom) { Console.WriteLine("Палиндром"); }
+	else{Console.WriteLine("нет");}
+}
---
+++ task21/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 21: Напишите программу, которая принимает 
+//на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+
+using System.Drawing;
+using System.Reflection.Metadata;
+
+int[] sum = new int[3];
+int tmp;
+
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 3; j++)
+    {
+        Console.WriteLine($"Точка {i + 1}. Введитеи координату {j}");
+        Int32.TryParse(Console.ReadLine(), out tmp);
+        sum[j] -= tmp;
+    }
+}
+
+Console.WriteLine(Math.Sqrt(Math.Pow(sum[0], 2) + Math.Pow(sum[1], 2) + Math.Pow(sum[2], 2)));
