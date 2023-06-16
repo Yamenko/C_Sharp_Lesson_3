d72865dd19995eb4f5f904cbac396f3367b25cb3
Me <1@1.ru>
2023-06-17 00:06:08 +0300

    Задание 23

---
+++ task23/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 4, 9. 
+//5 -> 1, 8, 27, 64, 125
+
+
+int num = 0;
+
+
+while (true)
+{
+    Console.WriteLine("Введите число (0 для выхода): ");
+
+    // Получаем число
+    if (!Int32.TryParse(Console.ReadLine(), out num)) { Console.WriteLine("Не число!"); continue; }
+
+    // Условие выхода
+    if (num == 0) { break; }
+
+    string str = "";
+    for (int i = 1; i <= num; i++) {
+        str += Math.Pow(i, 3) + ", ";
+    }
+    Console.WriteLine(str);
+}
